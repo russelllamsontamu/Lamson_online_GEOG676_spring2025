90bd82c8baecefa88491e3ccc7376d80e4544ac9
russelllamsontamu <russell_lamson@tamu.edu>
2025-02-02 21:47:26 -0600

    Update lab2.py
    finished writing comments about the code and what it will/should do

--- Labs/Lab2/lab2.py
+++ Labs/Lab2/lab2.py
@@ -7,7 +7,10 @@ number = 1
 for i in part1:
     number = number * i 
 
-print ('The result of the first question is:', number)
+# "i" represents the index within the loop and the word "for" creates the loop.
+# This loop will run until it runs out of indexs within the loop "part1"
+
+print ('The result of the first question after multiplying all of the numbers in the list together is:', number)
 
 #Part 2: Take the following list and add all list items together.
 
@@ -17,8 +20,10 @@ number2 = 0
 
 for i in part2:
     number2 = number2 + i 
+# "i" represents the index within the loop and the word "for" creates the loop.
+# This loop will run until it runs out of indexs within the loop "part2"
 
-print ('The resutl of the second question is:', number2)
+print ('The result of the second question after adding all of the numbers in the list together is:', number2)
 
 #Part 3: Take the following list and only add together those list items which are even.
 
@@ -29,6 +34,9 @@ number3 = 0
 for i in part3:
     if i % 2 == 0:
         number3 = number3 + i
-    
-print('The result of question 3 is:', number3)
+# "i" represents the index within the loop and the word "for" creates the loop.
+# This loop will run until it runs out of indexs with the loop "part3"
+# The "i % 2" formula will allow us to see if the index has a remainder or not
+# If there is no remainder, which is represented by " == 0", then the number in the index is even and will be used for the loop
 
+print('The result of the third question after adding only the even numbers in the list together is:', number3)
